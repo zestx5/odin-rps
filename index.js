711839660c95c0187d26292da3555e54a271c6be
zestx5 <buyankov.ilya@gmail.com>
2023-08-04 17:47:44 +0300

    add play single round function

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 function getComputerChoice() {
-    // Randomly return rock, paper or scissors
-    const choice = ['rock','paper','scissors'];
-    // Get integer 0..2
-    const index = Math.floor(Math.random() * 3);
+  // Randomly return rock, paper or scissors
+  const choice = ["rock", "paper", "scissors"];
+  // Get integer 0..2
+  const index = Math.floor(Math.random() * 3);
 
-    return choice[index];
+  return choice[index];
+}
+
+function playSingleRound(playerSelection, computerSelection) {
+  // make players choice case insensitive
+  let playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "Tie !";
+  }
+
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return `You Win! ${playerSelection.replace(/\b\w/g, (char) =>
+      char.toUpperCase()
+    )} beats ${computerSelection.replace(/\b\w/g, (char) =>
+      char.toUpperCase()
+    )}`;
+  }
+  return `You Lose! ${computerSelection.replace(/\b\w/g, (char) =>
+    char.toUpperCase()
+  )} beats ${playerSelection.replace(/\b\w/g, (char) => char.toUpperCase())}`;
 }
